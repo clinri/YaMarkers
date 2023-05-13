af596233423c8991c449aedec94d1e0fa234f37d
Andrei Borisov <clinri@ya.ru>
2023-05-13 22:15:38 +0300

    add map fragment and navigation-fragment-ktx dependency

---
+++ app/src/main/java/ru/netology/yamarkers/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.netology.yamarkers.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.yamarkers.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
---
+++ app/src/main/java/ru/netology/yamarkers/ui/MapFragment.kt
@@ -0,0 +1,49 @@
+package ru.netology.yamarkers.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.mapview.MapView
+import ru.netology.yamarkers.databinding.MapFragmentBinding
+
+class MapFragment : Fragment() {
+
+    private var mapView: MapView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MapKitFactory.initialize(requireContext())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = MapFragmentBinding.inflate(inflater, container, false)
+        mapView = binding.map
+
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+        MapKitFactory.getInstance().onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView = null
+    }
+}
