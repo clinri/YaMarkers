c7c1942821708c65eaf9350ccb785fda74e7ee10
Andrei Borisov <clinri@ya.ru>
2023-05-13 22:32:27 +0300

    implement move map to user location and getting permission in Manifest.permission.ACCESS_FINE_LOCATION

--- app/src/main/java/ru/netology/yamarkers/ui/MapFragment.kt
+++ app/src/main/java/ru/netology/yamarkers/ui/MapFragment.kt
@@ -1,19 +1,55 @@
 package ru.netology.yamarkers.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.user_location.UserLocationLayer
+import ru.netology.yamarkers.R
 import ru.netology.yamarkers.databinding.MapFragmentBinding
 
 class MapFragment : Fragment() {
 
     private var mapView: MapView? = null
+    private lateinit var userLocation: UserLocationLayer
+
+    private val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            when {
+                granted -> {
+                    userLocation.isVisible = true
+                    userLocation.isHeadingEnabled = false
+                    userLocation.cameraPosition()?.target?.also {
+                        val map = mapView?.map ?: return@registerForActivityResult
+                        val cameraPosition = map.cameraPosition
+                        map.move(
+                            CameraPosition(
+                                it,
+                                cameraPosition.zoom,
+                                cameraPosition.azimuth,
+                                cameraPosition.tilt,
+                            )
+                        )
+                    }
+                }
+                else -> {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.need_permission),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +62,17 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = MapFragmentBinding.inflate(inflater, container, false)
-        mapView = binding.map
+        mapView = binding.map.apply {
+            userLocation = MapKitFactory.getInstance().createUserLocationLayer(mapWindow)
+            if (requireActivity()
+                    .checkSelfPermission(
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                userLocation.isVisible = true
+                userLocation.isHeadingEnabled = false
+            }
+        }
 
         binding.plus.setOnClickListener {
             binding.map.map.move(
@@ -48,6 +94,10 @@ class MapFragment : Fragment() {
                 Animation(Animation.Type.SMOOTH, 0.3F),
                 null,
             )
+        }
+
+        binding.location.setOnClickListener {
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
 
         return binding.root
